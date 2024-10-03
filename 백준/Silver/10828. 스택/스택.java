f2c27e5b2182da95b5bb60b423ad0ef51b19ae20
JANG WOORIM <128919405+woorimj@users.noreply.github.com>
2024-10-03 14:22:48 +0900

    [Silver IV] Title: 스택, Time: 264 ms, Memory: 19128 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -31,7 +31,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
         stack = new int[N];
