c82b28c7fa9e4e5aea313b07e2a0bfffe4e7cf44
JANG WOORIM <128919405+woorimj@users.noreply.github.com>
2025-03-04 20:23:37 +0900

    [Silver II] Title: 섬의 개수, Time: 140 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -5,29 +5,33 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int h,w;
+    public static int h,w;
+    public static int[][] land = new int[50][50];
 
-    static int[][] land;
-    static boolean[][] visited;
 
-    // 방향백터 {좌,우,상,하,왼쪽위,왼쪽아래,오른쪽위,오른쪽아래}
-    static int[] dx = {-1, 1, 0, 0, -1, 1, -1, 1};
-    static int[] dy = {0, 0, 1, -1, -1, -1, 1, 1};
-
-    public static void dfs(int x, int y){
-        visited[x][y] = true;
-
-        for(int i = 0; i < 8; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            // 경계값을 기준으로 -> 아직 방문하지 않았으며, 연결된 1인 경우만 탐색
-            if (nx >= 0 && nx < h && ny >= 0 && ny < w) {
-                if (land[nx][ny] == 1 && !visited[nx][ny]) {
-                    dfs(nx, ny);
-                }
-            }
+    public static boolean dfs(int x, int y){
+        // 범위 나가면 종료
+        if(x <= -1 || x >= h || y <= -1 || y >= w){
+            return false;
         }
+
+        // 현재 노드가 1(땅)이면서 아직방문하지 않았다면
+        if(land[x][y] == 1){
+            land[x][y] = -1; // 방문처리함
+            // 좌,우
+            dfs(x - 1, y);
+            dfs(x + 1, y);
+            // 상,하
+            dfs(x , y - 1);
+            dfs(x, y + 1);
+            // 대각선
+            dfs(x - 1, y - 1);
+            dfs(x + 1, y - 1);
+            dfs(x - 1, y + 1);
+            dfs(x + 1, y + 1);
+            return true;
+        }
+        return false;
     }
 
 
@@ -35,22 +39,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         while(true){
-            int count = 0; // 총 섬의 그룹수
 
-            // 1. 지도 정보(w:너비, h:높이)
+            // 1.지도 정보(w:너비, h:높이)
             StringTokenizer st = new StringTokenizer(br.readLine());
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
 
-            // 종료조건 (0 0)
             if(w == 0 && h == 0)
                 return;
 
-            // 2. 초기화
-            land = new int[h][w];
-            visited = new boolean[h][w];
-
-            // 3.지도 정보입력
+            // 2.지도 정보입력
             for(int i = 0; i < h; i++){
                 st = new StringTokenizer(br.readLine());
                 for(int j = 0; j < w; j++){
@@ -58,17 +56,17 @@ public class Main {
                 }
             }
 
-            // 4.DFS 지도탐색
+            // 3.방문처리
+            int count = 0; // 섬의 개수
             for(int i = 0; i < h; i++){
                 for(int j = 0; j < w; j++){
-                    if(land[i][j] == 1 && !visited[i][j]){
-                        dfs(i,j);
-                        count++;
-                    }
+                   if(dfs(i,j)){
+                       count += 1;
+                   }
                 }
             }
 
-            // 5.출력
+            // 4.출력
             System.out.println(count);
         }
     }
