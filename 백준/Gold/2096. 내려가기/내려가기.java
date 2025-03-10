22b46f50ff9cf8a06af8631b854e1d9ca68c7cb3
JANG WOORIM <128919405+woorimj@users.noreply.github.com>
2025-03-10 23:24:02 +0900

    [Gold V] Title: 내려가기, Time: 400 ms, Memory: 49596 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,45 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N, max=0, min=0;
+    static int[][] array;
+    static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        int[][] board = new int[N + 1][3];
-        for (int i = 1; i <= N; i++) {
+        // 점수 정보입력
+        array = new int[N][3];
+        for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < 3; j++){
+                array[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] max = new int[N + 1][3];
-        int[][] min = new int[N + 1][3];
-        for (int i = 1; i <= N; i++) {
-            max[i][0] = Math.max(max[i - 1][0], max[i - 1][1]) + board[i][0];
-            max[i][1] = Math.max(max[i - 1][0], Math.max(max[i - 1][1], max[i - 1][2])) +  board[i][1];
-            max[i][2] = Math.max(max[i - 1][2], max[i - 1][1]) + board[i][2];
+        // 최대,최소 점수합 구하기
+        dp = new int[N][3];
 
-            min[i][0] = Math.min(min[i - 1][0], min[i - 1][1]) + board[i][0];
-            min[i][1] = Math.min(min[i - 1][0], Math.min(min[i - 1][1], min[i - 1][2])) + board[i][1];
-            min[i][2] = Math.min(min[i - 1][2], min[i - 1][1]) + board[i][2];
-        }
+        // 첫번째줄은 배열에 넣어놓기
+        for (int j = 0; j < 3; j++)
+            dp[0][j] = array[0][j];
 
-        int result_max = 0;
-        int result_min = 1000000 * 9; // 최대 숫자가 9이니깐?
-        for (int j = 0; j < 3; j++) {
-            if (result_max < max[N][j]) {
-                result_max = max[N][j];
-            }
-            if (result_min > min[N][j]) {
-                result_min = min[N][j];
-            }
+        // 두번째줄부터 max값 계산
+        for(int i = 1; i < N; i++){
+            dp[i][0] = array[i][0] + Math.max(dp[i-1][0],dp[i-1][1]);
+            dp[i][1] = array[i][1] + Math.max(Math.max(dp[i-1][0],dp[i-1][1]), dp[i-1][2]);
+            dp[i][2] = array[i][2] + Math.max(dp[i-1][1],dp[i-1][2]);
         }
-        System.out.print(result_max + " " + result_min);
+        max = Math.max(Math.max(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
+
+        // 최소합 구하기 위한 dp배열 초기화
+        for (int i = 1; i < N; i++)
+            Arrays.fill(dp[i], 0);
+
+        // 두번째줄부터 min값 계산
+        for(int i = 1; i < N; i++){
+            dp[i][0] = array[i][0] + Math.min(dp[i-1][0],dp[i-1][1]);
+            dp[i][1] = array[i][1] + Math.min(Math.min(dp[i-1][0],dp[i-1][1]), dp[i-1][2]);
+            dp[i][2] = array[i][2] + Math.min(dp[i-1][1],dp[i-1][2]);
+        }
+        min = Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
+
+        System.out.print(max + " " + min);
     }
 }
